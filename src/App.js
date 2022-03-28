960451ddf19cd714e76e9fc5a42c496916a243af
Recep Terzi <recepterzi-67@hotmail.com>
2022-03-28 11:53:22 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState(new Values('#f15025').all(5));
+  const [list, setList] = useState(new Values('#f15025').all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
